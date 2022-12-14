de4c2058632c6c19564c16392e822ca320da1b49
Antony Cartwright <antonyccartwright@gmail.com>
2022-12-15 00:48:54 +0100

    Generate list items

--- build.sh
+++ build.sh
@@ -14,10 +14,12 @@ ls -lrt './build'
 
 cat './index.html' | sed '$d' | sed '$d' | sed '$d' | sed '$d' > './build/index.html'
 cd './build'
+echo "<ol>" >> './index.html'
 ls -1 *.md | while read line; do
-  echo "<a href=\"$line\">$line</a><br/>" >> './index.html'
+  echo "<li><a href=\"$line\">$line</a></li>" >> './index.html'
   echo "$line" >> './index.txt'
 done
+echo "</ol>" >> './index.html'
 echo "</body>" >> './index.html'
 echo "</html>" >> './index.html'
 cat './index.html'
